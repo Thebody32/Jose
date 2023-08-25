39687bfee19c52af9fa0dbe400ce2426fdbe13f6
Thebody32 <143148096+Thebody32@users.noreply.github.com>
2023-08-24 21:59:14 -0400

    Update Lab2A.java

--- Lab2A.java
+++ Lab2A.java
@@ -12,30 +12,29 @@ import java.util.Scanner;
 class Lab2A {
     public static void main(String[] args) {
         String name1; //asks for first name
-        System.out.print("Enter a name: ");
+        System.out.println("Enter a name:");
         Scanner gg = new Scanner(System.in);
         name1 = gg.next();
 
         String name2; // asks for another name
-        System.out.print("Enter another name: ");
+        System.out.println("Enter another name:");
         Scanner mm = new Scanner(System.in);
         name2 = mm.next();
 
         String verb; // asks for verb
-        System.out.print("Enter a verb: ");
+        System.out.println("Enter a verb:");
         Scanner ss = new Scanner(System.in);
         verb = ss.next();
 
         String adverb; // asks for adverb
-        System.out.println("Enter an adverb: ");
+        System.out.println("Enter an adverb:");
         Scanner aa = new Scanner(System.in);
         adverb = aa.next();
 
-        System.out.print(" Once upon a time, there was a person named ");
-        System.out.print(name1);
-        System.out.print(" who had a child named ");
-        System.out.print(name2);
-        System.out.print(". This child would " + verb + " " + adverb + " while singing to strangers.");
+        System.out.println("Once upon a time, there was a person named " + name1 + " who had a child named "
+        + name2 + "." + " This child would " + verb + " " + adverb + " while singing to strangers.");
+
 
     }
 }
+
