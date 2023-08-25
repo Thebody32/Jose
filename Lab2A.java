9a60577cc97478d05fef87f0523d40ab092dbde8
Thebody32 <143148096+Thebody32@users.noreply.github.com>
2023-08-24 21:52:25 -0400

    Update Lab2A.java

--- Lab2A.java
+++ Lab2A.java
@@ -12,22 +12,22 @@ import java.util.Scanner;
 class Lab2A {
     public static void main(String[] args) {
         String name1; //asks for first name
-        System.out.print("Enter a name:");
+        System.out.print("Enter a name: ");
         Scanner gg = new Scanner(System.in);
         name1 = gg.next();
 
         String name2; // asks for another name
-        System.out.print(" Enter another name:");
+        System.out.print("Enter another name: ");
         Scanner mm = new Scanner(System.in);
         name2 = mm.next();
 
         String verb; // asks for verb
-        System.out.print(" Enter a verb:");
+        System.out.print("Enter a verb: ");
         Scanner ss = new Scanner(System.in);
         verb = ss.next();
 
         String adverb; // asks for adverb
-        System.out.println(" Enter an adverb:");
+        System.out.println("Enter an adverb: ");
         Scanner aa = new Scanner(System.in);
         adverb = aa.next();
 
@@ -35,7 +35,7 @@ class Lab2A {
         System.out.print(name1);
         System.out.print(" who had a child named ");
         System.out.print(name2);
-        System.out.print(". This child would " + verb + " " + adverb + " while singing to strangers");
+        System.out.print(". This child would " + verb + " " + adverb + " while singing to strangers.");
 
     }
 }
